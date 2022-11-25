7c38a3e6ae87cc35a4670827110155b6d7b84664
poylib <poy_lib@naver.com>
2022-11-25 13:37:38 +0900

    Add 링크 테스트

--- src/pages/Text.tsx
+++ src/pages/Text.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Text = () => {
   return (
     <>
@@ -5,6 +7,7 @@ const Text = () => {
       <h1>두번째 배포</h1>
       <h1>세번째 배포</h1>
       <h1>레포지토리 배포 확인 후 재배포 테스트</h1>
+      <Link to='/search'>검색 페이지로 이동</Link>
     </>
   );
 };
