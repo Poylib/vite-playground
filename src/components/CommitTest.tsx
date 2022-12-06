f93bfe4ed2b00c7745327b339cf891868c387d8f
poylib <poy_lib@naver.com>
2022-12-06 23:23:21 +0900

    Add commit test1

--- src/components/CommitTest.tsx
+++ src/components/CommitTest.tsx
@@ -1,10 +1,14 @@
 const CommitTest = () => {
   return (
     <>
+<<<<<<< HEAD
       <h1>시작 커밋</h1>
       <h1>중간 커밋 1</h1>
       <h1>중간 커밋 2</h1>
       <h1>최종 커밋</h1>
+=======
+      <h1>rebase 연습을 위한 쓸모없는 커밋 남기기</h1>
+>>>>>>> d59ccf7 (Add commit test1)
     </>
   );
 };
