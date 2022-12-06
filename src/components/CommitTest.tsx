4b2c5553be8205f0c855e876a500346244599e84
poylib <poy_lib@naver.com>
2022-12-06 23:33:47 +0900

    Add 시작 커밋

--- src/components/CommitTest.tsx
+++ src/components/CommitTest.tsx
@@ -1,9 +1,7 @@
 const CommitTest = () => {
   return (
     <>
-      <h1>rebase 연습을 위한 쓸모없는 커밋 남기기</h1>
-      <h1>rebase 연습을 위한 쓸모없는 커밋 남기기 2</h1>
-      <h1>rebase 연습을 위한 쓸모없는 커밋 남기기 3</h1>
+      <h1>시작 커밋</h1>
     </>
   );
 };
