3560650f66f623d768c89dea378be493cb0c2dc9
poylib <poy_lib@naver.com>
2022-12-09 21:13:22 +0900

    Add firebase 테스트 페이지로 가기위한 Link추가

--- src/pages/Text.tsx
+++ src/pages/Text.tsx
@@ -9,6 +9,7 @@ const Text = () => {
       <h1>레포지토리 배포 확인 후 재배포 테스트</h1>
       <h1>특정 브런치로 배포하기 테스트 - production deployment 수정을 위해 재배포</h1>
       <Link to='/search'>검색 페이지로 이동</Link>
+      <Link to='/fb'>firebase Test</Link>
     </>
   );
 };
