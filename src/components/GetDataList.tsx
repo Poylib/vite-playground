60a29c187fc8da26a4993ce7e4334cdf46af27ae
poylib <poy_lib@naver.com>
2022-12-09 18:53:21 +0900

    Add firebase에서 데이터 받아오기 getDocs를 이용해 문서에 저장된 데이터 불러옴
    state로 저장해 받아온 데이터 뿌리기

--- src/components/GetDataList.tsx
+++ src/components/GetDataList.tsx
@@ -1,30 +1,38 @@
 import db from '../firebase';
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import { UserData } from '../interface';
 
-const GetDataList = () => {
-  const [nameList, setNameList] = useState(['list']);
+function GetDataList() {
+  const [nameList, setNameList] = useState<UserData[]>([]);
+
   useEffect(() => {
     (async () => {
-      // const docRef = doc(db, 'users');
-      const docRefs = collection(db, 'users');
-      const docSnap = await getDocs(docRefs);
-      docSnap.forEach(doc => {
-        // console.log(doc.id, ' => ', doc.data());
-        const { data } = doc.data();
-        setNameList([data]);
+      let dataArr: UserData[] = [];
+      const querySnapshot = await getDocs(collection(db, 'users'));
+      querySnapshot.docs.map(el => {
+        const { data } = el.data();
+        dataArr.push(data);
       });
+      setNameList(dataArr);
     })();
   }, []);
   return (
     <>
       <ul>
-        {nameList.map((name, index) => {
-          return <li key={index}>{name}</li>;
+        {nameList.map((list, index) => {
+          return (
+            <li key={index}>
+              <p>
+                name: {list.name} <br />
+                age: {list.age}
+              </p>
+            </li>
+          );
         })}
       </ul>
     </>
   );
-};
+}
 
 export default GetDataList;
